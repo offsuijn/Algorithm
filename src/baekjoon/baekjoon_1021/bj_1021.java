c9a7503e9f045ec412181ea3fc88169716c72f4e
offsujin <offsujin@gmail.com>
2021-10-16 22:51:10 +0900

    bj 1021

--- src/baekjoon/baekjoon_1021/bj_1021.java
+++ src/baekjoon/baekjoon_1021/bj_1021.java
@@ -3,7 +3,6 @@ package baekjoon.baekjoon_1021;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Deque;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
@@ -16,8 +15,8 @@ public class bj_1021 {
 
         int N = Integer.parseInt(st.nextToken()); // 큐의 크기
         int M = Integer.parseInt(st.nextToken()); // 뽑아내려고 하는 수의 개수
-        int cnt = 0; // 2번, 3번 연산의 횟수
-        Deque<Integer> deque = new LinkedList<>();
+        int cnt = 0; // 2번, 3번 연산의 합
+        LinkedList<Integer> deque = new LinkedList<Integer>();
 
         // deque 초기화
         for (int i = 1; i <= N; i++) {
@@ -26,13 +25,39 @@ public class bj_1021 {
 
         st = new StringTokenizer(br.readLine());
         while (M-- > 0) {
-            int target = Integer.parseInt(st.nextToken());
-            while (target != deque.peekFirst()) {
-                deque.
+            int target = Integer.parseInt(st.nextToken()); // 뽑아내려고 하는 수
+
+            if (target != deque.peekFirst()) {
+                int k = deque.indexOf(target) + 1; // 뽑아내려고 하는 수의 위치
+                int center = 0; // 큐의 가운데
+
+                if (deque.size() % 2 == 1) { // size가 홀수인 경우
+                    center = deque.size() / 2 + 1;
+                }
+                else { // size가 짝수인 경우
+                    center = deque.size() / 2;
+                }
+
+                if (k <= center) { // 뽑아내려고 하는 수가 가운데이거나 가운데보다 앞에 있는 경우
+                    int l = k - 1;
+                    while (l-- > 0) { // target이 가장 앞에 오도록 왼쪽으로 한 칸 이동
+                        cnt++;
+                        deque.offerLast(deque.pollFirst());
+                    }
+                }
+                else { // 뽑아내려고 하는 수가 가운데보다 뒤에 있는 경우
+                    int l = deque.size() - k + 1;
+                    while (l-- > 0) { // target이 가장 앞에 오도록 오른쪽으로 한 칸 이동
+                        cnt++;
+                        deque.offerFirst(deque.pollLast());
+                    }
+                }
             }
             deque.pollFirst();
         }
 
+        sb.append(cnt);
+        System.out.println(sb);
     }
 
 }
