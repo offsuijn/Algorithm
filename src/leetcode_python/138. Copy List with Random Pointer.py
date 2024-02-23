7849e8c95d6bc5af2678f6e3338ee9a813055ab4
offsujin <88263178+offsujin@users.noreply.github.com>
2024-02-23 23:22:09 +0900

    Create 138. Copy List with Random Pointer.py

---
+++ src/leetcode_python/138. Copy List with Random Pointer.py
@@ -0,0 +1,21 @@
+# 138. Copy List with Random Pointer
+# https://leetcode.com/problems/copy-list-with-random-pointer/description/
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        oldToCopy = {None:None}
+
+        cur = head
+        while cur:
+            copy = Node(cur.val)
+            oldToCopy[cur] = copy
+            cur = cur.next
+
+        cur = head
+        while cur:
+            copy = oldToCopy[cur]
+            copy.next = oldToCopy[cur.next] 
+            copy.random = oldToCopy[cur.random] 
+            cur = cur.next
+
+        return oldToCopy[head]
