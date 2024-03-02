3f358aaa10aab67a0d9c1e18300a96c1c58489a3
offsujin <88263178+offsujin@users.noreply.github.com>
2024-03-02 17:27:26 +0900

    Create 162. Find Peak Element.py

---
+++ src/leetcode_python/162. Find Peak Element.py
@@ -0,0 +1,15 @@
+# 162. Find Peak Element
+# https://leetcode.com/problems/find-peak-element/description/
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = left + (right - left) // 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+                
+        return left
