5baca5fcae727485264a49002f11d6934f2ec7fa
offsujin <88263178+offsujin@users.noreply.github.com>
2024-03-19 22:06:39 +0900

    Update 931. Minimum Falling Path Sum.py

--- src/leetcode_python/931. Minimum Falling Path Sum.py
+++ src/leetcode_python/931. Minimum Falling Path Sum.py
@@ -2,24 +2,11 @@
 
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        if len(matrix) == 1:
-            return matrix[0][0]
-          
-        n = len(matrix)
-        dp = [[0] * n for _ in range(n)]
-        dp = matrix.copy() 
-      
-        for i in range(n - 2, -1, -1):
-            for j in range(n):
-                min_path = dp[i + 1][j]
-                if j > 0:
-                    min_path = min(min_path, dp[i + 1][j - 1])
-                if j < n - 1:
-                    min_path = min(min_path, dp[i + 1][j + 1])
-                dp[i][j] += min_path
-
-        result = float('inf')
-        for num in dp[0]:
-            result = min(result, num)
-          
-        return result
+        M, N = len(matrix), len(matrix[0])
+        for i in range(1, M):
+            for j in range(N):
+                a = matrix[i-1][j-1] if 0 < j else float('inf')
+                b = matrix[i-1][j]
+                c = matrix[i-1][j+1] if j < N - 1 else float('inf')
+                matrix[i][j] += min(a, b, c)
+        return min(matrix[-1])
