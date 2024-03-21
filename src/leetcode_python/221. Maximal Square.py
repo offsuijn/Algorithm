68077c70de913b156fb26cd458e277a8f1ee4a6e
offsujin <88263178+offsujin@users.noreply.github.com>
2024-03-21 09:55:09 +0900

    Update 221. Maximal Square.py
    refactoring by 2D Array DP

--- src/leetcode_python/221. Maximal Square.py
+++ src/leetcode_python/221. Maximal Square.py
@@ -2,12 +2,20 @@
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        m, n, result = len(matrix), len(matrix[0]), 0
-      
-        dp = [0] * n
+        m, n = len(matrix), len(matrix[0])
+        result = 0
+        curr, prev = [0]*n, [0]*n
+
         for i in range(m):
-            prev = 0
             for j in range(n):
-                dp[j], prev = 0 if matrix[i][j] == '0' else (min(dp[j], dp[j-1] if j > 0 else 0, prev)+ 1), dp[j]
-                result = dp[j] if dp[j] > result else result
-        return result * result
+                if matrix[i][j] == '1':
+                    curr[j] = min(curr[j-1] if j > 0 else 0,
+                                prev[j],
+                                prev[j-1] if j > 0 else 0) + 1
+                else:
+                    curr[j] = 0 # reset curr[j]
+                if result < curr[j]:
+                    result = curr[j]
+            prev, curr = curr, prev
+        
+        return result*result
