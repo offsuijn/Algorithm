c5e4fe5ec8dc1ad8d5b854971c760f923163d185
offsujin <88263178+offsujin@users.noreply.github.com>
2024-04-18 17:03:51 +0900

    Solve K번째수.py

---
+++ src/programmers/K번째수.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42748
+
+def solution(array, commands):
+    answer = []
+    
+    for i, j, k in commands:
+        answer.append(sorted(array[i-1:j])[k-1])
+        
+    return answer
