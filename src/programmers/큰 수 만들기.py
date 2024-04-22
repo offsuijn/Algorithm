925e5fc7d0eefe7f7287b50ddd7de220d5777b9c
offsujin <88263178+offsujin@users.noreply.github.com>
2024-04-22 20:46:26 +0900

    Solve 큰 수 만들기.py

---
+++ src/programmers/큰 수 만들기.py
@@ -0,0 +1,15 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42883
+
+def solution(number, k):
+    stack = []
+    
+    for num in number:
+        while stack and num > stack[-1] and k > 0:
+            stack.pop()
+            k-=1
+        stack.append(num)
+    
+    if k > 0:
+        stack = stack[:-k]
+    
+    return ''.join(stack)
