743c677a13799d61060e81d07dff7734ba544f0d
offsujin <88263178+offsujin@users.noreply.github.com>
2024-05-07 19:38:52 +0900

    Solve 전화번호 목록.py

---
+++ src/programmers/전화번호 목록.py
@@ -0,0 +1,11 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/42577#qna
+
+def solution(phone_book):
+    
+    phone_book.sort()
+    
+    for i in range(1, len(phone_book)):
+        if phone_book[i].startswith(phone_book[i-1]):
+            return False
+                   
+    return True
