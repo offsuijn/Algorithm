bd3556c24ef2986a2fb8b3b079e06cc27d4d1961
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-06-17 22:40:24 +0900

    Update 유사 칸토어 비트열.py

--- src/programmers/유사 칸토어 비트열.py
+++ src/programmers/유사 칸토어 비트열.py
@@ -1,17 +1,15 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/148652#
 
+def is_one(k):
+    while k >= 5:
+        if (k - 2) % 5 == 0:
+            return False
+        k //= 5
+    return k != 2
+
 def solution(n, l, r):
-    answer = 0
-
-    def f(idx):
-        if idx % 5 == 2:
-            return 0
-        elif idx < 5:
-            return 1
-        else:
-            return f(idx // 5)
-
-    for i in range(l-1, r):
-        answer += f(i)
-
-    return answer
+    cnt = 0
+    for k in range(l-1, r):
+        if is_one(k):
+           cnt += 1
+    return cnt
