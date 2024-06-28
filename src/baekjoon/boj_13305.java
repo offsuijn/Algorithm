47eac31214df46b2b0e033b7fdcdb930c4f77e3a
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-06-28 19:31:17 +0900

    Update boj_13305.java

--- src/baekjoon/boj_13305.java
+++ src/baekjoon/boj_13305.java
@@ -1,48 +1,28 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
-import java.util.StringTokenizer;
- 
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		long[] dist = new long[N - 1];	// 거리
-		long[] cost = new long[N];	// 비용 
-		
-		// 거리 입력 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < N - 1; i++) {
-			dist[i] = Long.parseLong(st.nextToken());
-		}
-		
-		// 리터당 기름값 입력
-		st = new StringTokenizer(br.readLine(), " ");
-		for(int i = 0; i < N; i++) {
-			cost[i] = Long.parseLong(st.nextToken());
-		}
-		
-		long sum = 0;
-		long minCost = cost[0];	// 이전 까지의 과정 중 주유 최소 비용 
- 
-		for(int i = 0; i < N - 1; i++) {
-			
-			/*
-			 *  현재 주유소가 이전 주유소의 기름값보다 쌀 경우
-			 *  minCost를 갱신해준다. 
-			 */
-			if(cost[i] < minCost) {
-				minCost = cost[i];
-			}
-			
-			sum += (minCost * dist[i]);
-		}
-		
-		System.out.println(sum);
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] distances = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+        long[] prices = Arrays.stream(br.readLine().split(" ")).mapToLong(Long::parseLong).toArray();
+
+        long cost = 0; // 총 비용
+        long minPrice = prices[0]; // 현재까지 가장 저렴한 주유 가격
+
+        // 첫 번째 도시에서 두 번째 도시로 이동하는 비용
+        cost += distances[0] * prices[0];
+
+        for (int i = 1; i < n-1; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            }
+            cost += distances[i] * minPrice;
+        }
+
+        System.out.println(cost);
+    }
 }
