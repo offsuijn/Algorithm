bf4c0c352c425fe9659a6bdd81b9fe6586b4d915
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-07-12 13:49:55 +0900

    Update 14501.py

--- src/boj_python/14501.py
+++ src/boj_python/14501.py
@@ -1,15 +1,14 @@
 # https://www.acmicpc.net/problem/14501
 
-import sys
+n = int(input())
 
-N = int(input())
-schedule = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+schedule = [list(map(int, input().split())) for _ in range(n)]
+dp = [0] * (n+1)
 
-dp = [0 for _ in range(N+1)]
+for i in range(n-1, -1, -1):
+    if (i + schedule[i][0]) <= n: # 스케줄을 선택할 경우
+        dp[i] = max(dp[i+1], dp[i + schedule[i][0]] + schedule[i][1])
+    else: # 스케줄을 선택하지 않을 경우
+        dp[i] = dp[i+1]
 
-for i in range(N):
-    for j in range(i + schedule[i][0], N + 1):
-        if dp[i] + schedule[i][1]:
-            dp[j] = dp[i] + schedule[i][1]
-
-print(dp[-1])
+print(dp[0])
