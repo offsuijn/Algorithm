8db05c16ba4c287a1ae2a3827856ad1c064e111b
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-07-12 13:18:33 +0900

    Update 12865.py 1차원 배열 풀이 추가

--- src/boj_python/12865.py
+++ src/boj_python/12865.py
@@ -1,8 +1,24 @@
 '''
+https://www.acmicpc.net/problem/12865
 12865번: 평범한 배낭
 => dp문제
 '''
 
+# 1차원 배열 풀이
+n, k = map(int, input().split())
+items = [list(map(int, input().split())) for _ in range(n)]
+
+dp = [0] * (k + 1)
+
+for i in range(n):
+    # dp 배열은 이전 상태이기 때문에 뒤에서부터 접근하여 이전 값이 오염되지 않도록 한다.
+    for j in range(k, items[i][0] - 1, -1):
+        if items[i][0] <= j:
+            dp[j] = max(dp[j], dp[j - items[i][0]] + items[i][1])
+            
+print(dp[-1])
+
+# 2차원 배열 풀이
 import sys
 
 N, K = tuple(map(int, sys.stdin.readline().split()))  # 물품의 수, 버틸 수 있는 무게
