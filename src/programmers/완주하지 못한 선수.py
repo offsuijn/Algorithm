237c03237d33865a0df7517562c9eabfd2367065
Sujin <88263178+diveintoo@users.noreply.github.com>
2024-08-04 20:39:47 +0900

    Update 완주하지 못한 선수.py Counter 사용

--- src/programmers/완주하지 못한 선수.py
+++ src/programmers/완주하지 못한 선수.py
@@ -8,3 +8,15 @@ def solution(participant, completion):
         if participant[i] != completion[i]:
             return participant[i]
     return participant[-1]
+
+# Using Counter
+from collections import Counter
+
+def solution(participant, completion):
+    part = Counter(participant)
+    comp = Counter(completion)
+    
+    non = part - comp # - 연산 -> 0 또는 음수 제거
+    # part.subtract(comp) # subtract -> 0 또는 음수 포함
+    
+    return ''.join(non.elements()) # elements() -> 0 또는 음수 제거
